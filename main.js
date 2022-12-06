2ea1b895e934f927f7a16bfbfc61139134c80073
Eduard <>
2022-12-06 11:29:02 -0500

    main.js changes, read and filter planets from the csv file

--- main.js
+++ main.js
@@ -1,16 +1,30 @@
 const {parse} = require('csv-parse');
 const fs = require('fs');
 
-const results = []
+const habitablePlanets = []
+
+function habitablePlanet(planet){
+    return planet['koi_disposition'] === 'CONFIRMED'
+    && planet['koi_insol']> 0.36 && planet['koi_insol']< 1.11 && planet['koi_prad']<1.6;
+}
 
 fs.createReadStream('kepler-data.csv')
+    .pipe(parse({
+        comment: '#',
+        columns: true,
+    }))
     .on('data',(data)=>{
-        results.push(data);
+        if(habitablePlanet(data)){
+            habitablePlanets.push(data);
+        }
     })
     .on('error', (err)=>{
         console.log(err);
     })
     .on('end', ()=>{
-        console.log(results);
-        console.log('Done');
+        console.log(`There are ${habitablePlanets.length} habitable planets found`);
+        console.log(habitablePlanets.map((planet)=>{
+            return planet['kepler_name'];
+        }));
     });
+
